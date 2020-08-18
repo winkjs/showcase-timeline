5d0db5230ba1c506af6842e69c8d47981b84c076
Prateek Saxena <prtksxna@gmail.com>
2020-08-18 13:27:18 +0530

    feat: add suggestions for articles to load

--- client.js
+++ client.js
@@ -3,7 +3,6 @@ var getTimeline = function (title) {
     .then(function (res) {
       return res.json();
     }).then(function (d) {
-      console.log(d);
       resetUI();
       showResults(d.timeline);
     })
@@ -12,6 +11,7 @@ var getTimeline = function (title) {
 var resetUI = function () {
   $('#timeline-button').prop('value', 'Create timeline');
   $('.lds-ripple').hide();
+  $('.timeline').show();
 }
 
 var showResults = function (timeline) {
@@ -19,7 +19,8 @@ var showResults = function (timeline) {
 
   if(timeline.length === 0){
     $('#error').show();
-    $('#error').text('There in no Wikipedia article with that title. Try a different one, for example "Delhi".')
+    $('.timeline').hide();
+    $('#error').html('There in no Wikipedia article with that title (maybe it has a different captilaziation).<br>Try a different one, for example <a href="javascript:void(0)" class="js-article-ex">Delhi</a>.');
     return;
   }
 
@@ -28,7 +29,6 @@ var showResults = function (timeline) {
 
   var lastYear = '';
   timeline.forEach((t) => {
-    console.log(t.date);
     if (isNaN(t.date)) {
       className = 'big';
     } else {
@@ -45,28 +45,36 @@ var showResults = function (timeline) {
       $('<div>').addClass('body').html(t.sentence),
     )
 
-
     $('.entries').append($entry);
   });
+}
 
+var loadArticle = function (title) {
+  // UI changes
+  $('#title').prop('value',title)
+  $('#error').hide();
+  $('#timeline-button').prop('value', 'Fetching timeline…');
+  $('.lds-ripple').show();
+  $('.timeline').hide();
+
+  // Make API call
+  getTimeline(title);
 }
 
 $(function () {
   // Warming up the server for the user's request
-  fetch('https://showcase-serverless.herokuapp.com/wp-timeline?title=Delhi');
+  fetch('https://showcase-serverless.herokuapp.com/wp-timeline?title=New York City');
 
   $('#timeline-button').on('click', function () {
-    $('#error').hide();
-
     // Check input string
     var title = $('#title')[0].value;
     if (title.trim() === '' ) return false;
 
-    // UI changes
-    $('#timeline-button').prop('value', 'Fetching timeline…');
-    $('.lds-ripple').show();
-
-    // Make API call
-    getTimeline(title);
+    loadArticle(title);
   });
+
+  $(document).on('click','.js-article-ex',function () {
+    var title = $(this).text();
+    loadArticle(title);
+  })
 })
